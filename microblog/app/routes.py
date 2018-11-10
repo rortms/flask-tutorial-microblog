336ee41f2e11ece27e8b7b36db481d9a0c7cc20a
rortms <rortms@gmail.com>
2018-11-09 19:54:08 -0800

    End of chapter 3

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import myapp
 from app.forms import LoginForm
 
@@ -21,7 +21,10 @@ def index():
     return render_template('index.html', title='Home', user=user, posts=posts)
 
 # Login View
-@myapp.route('/login')
+@myapp.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
