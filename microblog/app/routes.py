29214fed0322856cb43e50f407b1d04d33efb5b8
rortms <rortms@gmail.com>
2018-11-15 00:40:23 -0800

    End chapter 6

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -5,11 +5,13 @@ from flask_login import logout_user, login_required
 
 from app import myapp
 from app import db
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from app.models import User
 
 from flask import request            #
 from werkzeug.urls import url_parse  # For redirection after hitting a login_required page
+
+from datetime import datetime
 
 # Index View
 @myapp.route('/')
@@ -89,3 +91,28 @@ def user(username):
         {'author': user, 'body': 'Test post #2'}
     ]
     return render_template('user.html', user=user, posts=posts)
+
+# Edit Profile
+@myapp.route('/edit_profile', methods=["GET", "POST"])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash("Your changes have been saved")
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
+        
+
+# Record last time user interacts with site
+@myapp.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+        
