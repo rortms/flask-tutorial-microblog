151732599de47be5f1d77eaf1f50a7922f26032b
rortms <rortms@gmail.com>
2018-11-20 12:37:47 -0800

    Chapter 7, email error reporting setup and working.

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -4,15 +4,50 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
+import logging
+from logging.handlers import SMTPHandler
+
 myapp = Flask(__name__)
 
+#################
+# Load config class
 myapp.config.from_object(Config)
 
+
+#################
+# Load Database
 db = SQLAlchemy(myapp)
 migrate = Migrate(myapp, db)
 
+
+#################
+# Login Manager
 login = LoginManager(myapp)
 login.login_view = 'login' # The 'login' value here is the function (or endpoint) name for the login view
 
-from app import routes, models # forms.py is imported from within models.py 
 
+#################
+# Email bug reporting
+if not myapp.debug:
+    if myapp.config['MAIL_SERVER']:
+        auth = None
+        if myapp.config['MAIL_USERNAME'] or myapp.config['MAIL_PASSWORD']:
+            auth = (myapp.config['MAIL_USERNAME'], myapp.config['MAIL_PASSWORD'])
+        secure = None
+        if myapp.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost = (myapp.config['MAIL_SERVER'], myapp.config['MAIL_PORT']),
+            fromaddr='no-reply@' + myapp.config['MAIL_SERVER'],
+            toaddrs=myapp.config['ADMINS'], subject='Microblog Failure',
+            credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        myapp.logger.addHandler(mail_handler)
+
+        
+#################
+# Logging to File
+
+
+#
+from app import routes, models, errors # forms.py is imported from within models.py 
--- microblog/config.py
+++ microblog/config.py
@@ -6,8 +6,15 @@ class Config(object):
     # Authentication
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     
-    # # Database
+    # Database
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                               'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
+
+    # Email error reporting
+    MAIL_SERVER   = os.environ.get('MAIL_SERVER')  
+    MAIL_PORT     = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS  = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS        = ['doingsomedev@gmail.com']
